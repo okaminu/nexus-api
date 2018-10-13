edc87dafb7cd9620932d1b08e6adb3e5070cd2b6
Andrius Degutis <andriusdgt@gmail.com>
2018-10-13 20:38:41 +0300

    BA-465: attached our created validator to name field

--- src/main/kotlin/lt/boldadmin/nexus/api/type/entity/Project.kt
+++ src/main/kotlin/lt/boldadmin/nexus/api/type/entity/Project.kt
@@ -1,6 +1,7 @@
 package lt.boldadmin.nexus.api.type.entity
 
 import lt.boldadmin.nexus.api.type.valueobject.Address
+import lt.boldadmin.nexus.validator.UniqueProjectName
 import javax.validation.constraints.Min
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
@@ -10,6 +11,7 @@ class Project(
     var id: String? = null,
 
     @field:Size(min = 1, message = "Project name required")
+    @field:UniqueProjectName()
     var name: String = "",
 
     @field:NotNull(message = "Project address required")
