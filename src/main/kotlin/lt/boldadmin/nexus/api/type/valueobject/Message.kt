f46eb621325fd51517fc709c45428042d697070d
Okaminu <aurimasdgt@gmail.com>
2018-10-16 17:12:56 +0300

    BA-442: replaced empty message to symbol

--- src/main/kotlin/lt/boldadmin/nexus/api/type/valueobject/Message.kt
+++ src/main/kotlin/lt/boldadmin/nexus/api/type/valueobject/Message.kt
@@ -3,6 +3,6 @@ package lt.boldadmin.nexus.api.type.valueobject
 data class Message(
     val senderPhoneNumber: String,
     val collaboratorMobileNumber: String,
-    val projectName: String? = null,
+    val projectName: String,
     val description: String? = null
 )
