7840f63c41ae4d83bb62f9842e0e0652aa50062f
Okaminu <aurimasdgt@gmail.com>
2018-10-16 17:54:19 +0300

    BA-442: replaced project name and description with single contents field

--- src/main/kotlin/lt/boldadmin/nexus/api/type/valueobject/Message.kt
+++ src/main/kotlin/lt/boldadmin/nexus/api/type/valueobject/Message.kt
@@ -3,6 +3,5 @@ package lt.boldadmin.nexus.api.type.valueobject
 data class Message(
     val senderPhoneNumber: String,
     val collaboratorMobileNumber: String,
-    val projectName: String,
-    val description: String? = null
+    val content: String
 )
