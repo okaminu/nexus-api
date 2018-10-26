0c42affa775f8be68afeae5fc23da430bf2cdb25
Okaminu <aurimasdgt@gmail.com>
2018-10-26 16:20:16 +0300

    BA-442: fixes after code review

--- src/main/kotlin/lt/boldadmin/nexus/api/SmsGateway.kt
+++ src/main/kotlin/lt/boldadmin/nexus/api/SmsGateway.kt
@@ -1,5 +1,5 @@
 package lt.boldadmin.nexus.api
 
 interface SmsGateway {
-    fun send(fromPhoneNumber: String, toPhoneNumber: String, message: String)
+    fun send(senderPhoneNumber: String, receiverPhoneNumber: String, message: String)
 }
