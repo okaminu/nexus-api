f2f3f5e5933aafbb87451e18b73d8042d8e2d298
vsh1ft <kastytis.ch@gmail.com>
2018-12-11 02:34:37 +0200

    BA-764: renaming, xml config changes

--- src/main/kotlin/lt/boldadmin/nexus/api/service/CollaboratorUpdateEvent.kt
+++
@@ -1,7 +0,0 @@
-package lt.boldadmin.nexus.api.service
-
-import lt.boldadmin.nexus.api.type.entity.Collaborator
-
-interface CollaboratorUpdateEvent {
-    fun run(collaborator: Collaborator, attributeName: String, attributeValue: String)
-}
--- src/main/kotlin/lt/boldadmin/nexus/api/service/worklog/status/time/WorklogWorkTimeService.kt
+++
@@ -1,9 +0,0 @@
-package lt.boldadmin.nexus.api.service.worklog.status.time
-
-import lt.boldadmin.nexus.api.type.valueobject.TimeRange
-
-interface WorklogWorkTimeService {
-
-    fun logWork(collaboratorId: String, workTime: TimeRange)
-
-}
