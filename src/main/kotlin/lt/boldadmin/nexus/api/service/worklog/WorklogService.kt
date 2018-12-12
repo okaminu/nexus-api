33b21ec6d7ccf6923b8d1b2c494b9c1c9e457938
Okaminu <aurimasdgt@gmail.com>
2018-12-12 17:16:31 +0200

    BA-742: handlers for nexus-api services

--- src/main/kotlin/lt/boldadmin/nexus/api/service/worklog/WorklogService.kt
+++ src/main/kotlin/lt/boldadmin/nexus/api/service/worklog/WorklogService.kt
@@ -2,7 +2,7 @@ package lt.boldadmin.nexus.api.service.worklog
 
 import lt.boldadmin.nexus.api.type.entity.Worklog
 
-interface WorkLogService {
+interface WorklogService {
     fun getByCollaboratorId(id: String): Collection<Worklog>
     fun getByProjectId(id: String): Collection<Worklog>
     fun getIntervalEndpoints(intervalId: String): Collection<Worklog>
