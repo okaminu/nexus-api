9c36c7181e7dd9801b1dc208616d9d735d467bff
Okaminu <aurimasdgt@gmail.com>
2018-12-20 15:44:22 +0200

    BA-764: code review fixes

--- src/main/kotlin/lt/boldadmin/nexus/api/service/worklog/CollaboratorUpdateListener.kt
+++
@@ -1,7 +0,0 @@
-package lt.boldadmin.nexus.api.service.worklog
-
-import lt.boldadmin.nexus.api.type.entity.Collaborator
-
-interface CollaboratorUpdateListener {
-    fun run(collaborator: Collaborator, attributeName: String, attributeValue: String)
-}
