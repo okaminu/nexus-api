0656e8f39a1a7a129b2ebf61b915c761f17d4c68
Okaminu <aurimasdgt@gmail.com>
2019-01-19 01:13:03 +0200

    BA-742: worklogs can be saved directly in order for acceptance tests to setup initial data

--- src/main/kotlin/lt/boldadmin/nexus/api/service/worklog/WorklogService.kt
+++ src/main/kotlin/lt/boldadmin/nexus/api/service/worklog/WorklogService.kt
@@ -3,6 +3,7 @@ package lt.boldadmin.nexus.api.service.worklog
 import lt.boldadmin.nexus.api.type.entity.Worklog
 
 interface WorklogService {
+    fun save(worklog: Worklog)
     fun getByCollaboratorId(id: String): Collection<Worklog>
     fun getByProjectId(id: String): Collection<Worklog>
     fun getIntervalEndpoints(intervalId: String): Collection<Worklog>
