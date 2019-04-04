1bb1c5ab1ae8b0baa87a362877917e5ca367b2f3
vsh1ft <kastytis.ch@gmail.com>
2019-04-04 22:07:34 +0300

    BA-502: location update backend endpoint

--- src/main/kotlin/lt/boldadmin/nexus/api/service/ProjectService.kt
+++ src/main/kotlin/lt/boldadmin/nexus/api/service/ProjectService.kt
@@ -1,10 +1,12 @@
 package lt.boldadmin.nexus.api.service
 
 import lt.boldadmin.nexus.api.type.entity.Project
+import lt.boldadmin.nexus.api.type.valueobject.Location
 
 interface ProjectService {
     fun createWithDefaults(userId: String): Project
     fun getById(projectId: String): Project
     fun update(id: String, attributeName: String, attributeValue: String)
     fun updateOrderNumber(projectId: String, orderNumber: Short)
+    fun updateLocation(projectId: String, location: Location)
 }
