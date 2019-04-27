c3f44de87c3449ee8651b6d1de862130363f4b1d
willow <jurate.baltrukenaite@gmail.com>
2019-04-27 22:55:35 +0300

    BA-844: added routes to collaborator work durations sum

--- src/main/kotlin/lt/boldadmin/nexus/api/service/worklog/duration/WorklogDurationService.kt
+++ src/main/kotlin/lt/boldadmin/nexus/api/service/worklog/duration/WorklogDurationService.kt
@@ -3,4 +3,5 @@ package lt.boldadmin.nexus.api.service.worklog.duration
 interface WorklogDurationService {
     fun measureDuration(intervalId: String): Long
     fun sumWorkDurations(workLogIntervalIds: Collection<String>): Long
+    fun sumWorkDurationsByCollaboratorId(collaboratorId: String): Long
 }
