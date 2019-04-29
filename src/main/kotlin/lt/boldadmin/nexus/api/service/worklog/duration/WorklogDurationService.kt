a3b7a9219171944a0e5107ff85ab318fa415ba5f
willow <jurate.baltrukenaite@gmail.com>
2019-04-29 21:54:58 +0300

    BA-884: added routes for getting workingtime durations sum by project

--- src/main/kotlin/lt/boldadmin/nexus/api/service/worklog/duration/WorklogDurationService.kt
+++ src/main/kotlin/lt/boldadmin/nexus/api/service/worklog/duration/WorklogDurationService.kt
@@ -4,4 +4,5 @@ interface WorklogDurationService {
     fun measureDuration(intervalId: String): Long
     fun sumWorkDurations(workLogIntervalIds: Collection<String>): Long
     fun sumWorkDurationsByCollaboratorId(collaboratorId: String): Long
+    fun sumWorkDurationsByProjectId(projectId: String): Long
 }
