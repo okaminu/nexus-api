b1dbb1571c97de38656eb8b75fc603bc63d3723c
willow <jurate.baltrukenaite@gmail.com>
2019-05-02 11:35:22 +0300

    BA-884: added unit tests for continuing work duration sum, removed sumWorkDurations function

--- src/main/kotlin/lt/boldadmin/nexus/api/service/worklog/duration/WorklogDurationService.kt
+++ src/main/kotlin/lt/boldadmin/nexus/api/service/worklog/duration/WorklogDurationService.kt
@@ -2,7 +2,6 @@ package lt.boldadmin.nexus.api.service.worklog.duration
 
 interface WorklogDurationService {
     fun measureDuration(intervalId: String): Long
-    fun sumWorkDurations(workLogIntervalIds: Collection<String>): Long
     fun sumWorkDurationsByCollaboratorId(collaboratorId: String): Long
     fun sumWorkDurationsByProjectId(projectId: String): Long
 }
