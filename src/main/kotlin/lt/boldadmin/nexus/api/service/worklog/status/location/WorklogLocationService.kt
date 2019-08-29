03fb64fe4815b525ef87fbcaff389ed1c91988ec
Okaminu <aurimasdgt@gmail.com>
2019-08-29 17:37:12 +0300

    BA-818: replaced collaborator entity with id

--- src/main/kotlin/lt/boldadmin/nexus/api/service/worklog/status/location/WorklogLocationService.kt
+++ src/main/kotlin/lt/boldadmin/nexus/api/service/worklog/status/location/WorklogLocationService.kt
@@ -1,8 +1,7 @@
 package lt.boldadmin.nexus.api.service.worklog.status.location
 
-import lt.boldadmin.nexus.api.type.entity.Collaborator
 import lt.boldadmin.nexus.api.type.valueobject.Coordinates
 
 interface WorklogLocationService {
-    fun logWork(collaborator: Collaborator, collaboratorCoordinates: Coordinates)
+    fun logWork(collaboratorId: String, coordinates: Coordinates)
 }
