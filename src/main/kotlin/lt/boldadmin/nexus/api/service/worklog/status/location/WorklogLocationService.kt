46b44b607ce31aadb4887846a54bbce8c04e4009
Okaminu <aurimasdgt@gmail.com>
2019-09-05 19:45:26 +0300

    BA-818: moved location update by message and coordinates from worklog to collaborator event publisher

--- src/main/kotlin/lt/boldadmin/nexus/api/service/worklog/status/location/WorklogLocationService.kt
+++
@@ -1,7 +0,0 @@
-package lt.boldadmin.nexus.api.service.worklog.status.location
-
-import lt.boldadmin.nexus.api.type.valueobject.Coordinates
-
-interface WorklogLocationService {
-    fun logWork(collaboratorId: String, coordinates: Coordinates)
-}
