11f2f1a578a08a44de7f875908cd231f360cbfce
Okaminu <aurimasdgt@gmail.com>
2019-09-05 17:37:31 +0300

    BA-818: removed unused method

--- src/main/kotlin/lt/boldadmin/nexus/api/service/worklog/status/WorklogStartEndService.kt
+++ src/main/kotlin/lt/boldadmin/nexus/api/service/worklog/status/WorklogStartEndService.kt
@@ -1,16 +1,10 @@
 package lt.boldadmin.nexus.api.service.worklog.status
 
-import lt.boldadmin.nexus.api.type.entity.Collaborator
 import lt.boldadmin.nexus.api.type.entity.Project
 
 interface WorklogStartEndService {
     fun getProjectOfStartedWork(collaboratorId: String): Project
-    fun start(collaborator: Collaborator, project: Project)
-    fun start(collaborator: Collaborator, project: Project, timestamp: Long)
     fun hasWorkStarted(collaboratorId: String): Boolean
     fun hasWorkStarted(collaboratorId: String, projectId: String): Boolean
-    fun end(collaborator: Collaborator)
-    fun end(collaborator: Collaborator, timestamp: Long)
     fun endAllStartedWorkWhereWorkTimeEnded()
-    fun hasWorkEnded(collaboratorId: String): Boolean
 }
