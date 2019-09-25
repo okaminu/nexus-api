0f0a825df8f450358894e522c015968c7d751082
Okaminu <aurimasdgt@gmail.com>
2019-09-25 13:56:18 +0300

    BA-818: collaborator coordinates are being passed to frontend

--- src/main/kotlin/lt/boldadmin/nexus/api/type/entity/collaborator/CollaboratorCoordinates.kt
+++ src/main/kotlin/lt/boldadmin/nexus/api/type/entity/collaborator/CollaboratorCoordinates.kt
@@ -3,7 +3,7 @@ package lt.boldadmin.nexus.api.type.entity.collaborator
 import lt.boldadmin.nexus.api.type.valueobject.Coordinates
 
 class CollaboratorCoordinates(
-    val collaboratorId: String,
-    val coordinates: Coordinates,
-    val timestamp: Long
+    val collaboratorId: String = "",
+    val coordinates: Coordinates = Coordinates(),
+    val timestamp: Long = 123
 )
