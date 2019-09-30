7824a3ef433f476a7d76e74dfb94e90bc3481c44
vsh1ft <kastytis.ch@gmail.com>
2019-10-01 00:34:37 +0300

    BA-734: parameter type change to Coordinates

--- src/main/kotlin/lt/boldadmin/nexus/api/ReverseGeocodeConverter.kt
+++ src/main/kotlin/lt/boldadmin/nexus/api/ReverseGeocodeConverter.kt
@@ -1,9 +1,11 @@
 package lt.boldadmin.nexus.api
 
+import lt.boldadmin.nexus.api.type.valueobject.Coordinates
+
 interface ReverseGeocodeConverter {
 
-    fun convertToAddress(latitude: Double, longitude: Double): String
+    fun convertToAddress(coordinates: Coordinates): String
 
-    fun convertToPlusCode(latitude: Double, longitude: Double): String
+    fun convertToPlusCode(coordinates: Coordinates): String
 
 }
