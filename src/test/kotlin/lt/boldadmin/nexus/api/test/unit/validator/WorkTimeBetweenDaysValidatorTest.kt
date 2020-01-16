636e00e706ea7a7255744cb933feed50d95e082a
Andrius Degutis <andriusdgt@gmail.com>
2020-01-16 21:49:23 +0200

    BA-891: created new work time between days validator

---
+++ src/test/kotlin/lt/boldadmin/nexus/api/test/unit/validator/WorkTimeBetweenDaysValidatorTest.kt
@@ -0,0 +1,75 @@
+package lt.boldadmin.nexus.api.test.unit.validator
+
+import lt.boldadmin.nexus.api.type.valueobject.TimeRange
+import lt.boldadmin.nexus.api.validator.WorkTimeBetweenDaysValidator
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class WorkTimeBetweenDaysValidatorTest {
+
+    @Test
+    fun `Validation passes when time gap between work days time is longer than 10 minutes`() {
+        val workTime = Array(7) { TimeRange(100, 200) }
+
+        assertTrue(WorkTimeBetweenDaysValidator().isValid(workTime))
+    }
+
+    @Test
+    fun `Validation passes when time gap between work days time is equal to 10 minutes (5 + 5 minutes)`() {
+        val workTime = Array(7) { TimeRange(5, 1435) }
+
+        assertTrue(WorkTimeBetweenDaysValidator().isValid(workTime))
+    }
+
+    @Test
+    fun `Validation passes when time gap between work days time is equal to 10 minutes (3 + 7 minutes)`() {
+        val workTime = Array(7) { TimeRange(7, 1437) }
+
+        assertTrue(WorkTimeBetweenDaysValidator().isValid(workTime))
+    }
+
+    @Test
+    fun `Validation passes when time gap between work days time is equal to 10 minutes (7 + 3 minutes)`() {
+        val workTime = Array(7) { TimeRange(3, 1433) }
+
+        assertTrue(WorkTimeBetweenDaysValidator().isValid(workTime))
+    }
+
+    @Test
+    fun `Validation fails when time gap between work days is shorter than 10 minutes (3 + 3 minutes)`() {
+        val workTime = Array(7) { TimeRange(3, 1437) }
+        val sundayMondayWorkTime = arrayOf(TimeRange(3, 200)) + Array(6) { TimeRange(1400, 1437) }
+
+        assertFalse(WorkTimeBetweenDaysValidator().isValid(workTime))
+        assertFalse(WorkTimeBetweenDaysValidator().isValid(sundayMondayWorkTime))
+    }
+
+    @Test
+    fun `Validation fails when time gap between work days is shorter than 10 minutes (1 + 5 minutes)`() {
+        val workTime = Array(7) { TimeRange(5, 1439) }
+        val sundayMondayWorkTime = arrayOf(TimeRange(5, 200)) + Array(6) { TimeRange(1400, 1439) }
+
+        assertFalse(WorkTimeBetweenDaysValidator().isValid(workTime))
+        assertFalse(WorkTimeBetweenDaysValidator().isValid(sundayMondayWorkTime))
+    }
+
+    @Test
+    fun `Validation fails when time gap between work days is shorter than 10 minutes (5 + 1 minutes)`() {
+        val workTime = Array(7) { TimeRange(1, 1435) }
+        val sundayMondayWorkTime = arrayOf(TimeRange(1, 200)) + Array(6) { TimeRange(1400, 1435) }
+
+        assertFalse(WorkTimeBetweenDaysValidator().isValid(workTime))
+        assertFalse(WorkTimeBetweenDaysValidator().isValid(sundayMondayWorkTime))
+    }
+
+    @Test
+    fun `Validation fails when there is no time gap between work days time`() {
+        val workTime = Array(7) { TimeRange(0, 1440) }
+        val sundayMondayWorkTime = arrayOf(TimeRange(0, 100)) + Array(6) { TimeRange(1440, 1440) }
+
+        assertFalse(WorkTimeBetweenDaysValidator().isValid(workTime))
+        assertFalse(WorkTimeBetweenDaysValidator().isValid(sundayMondayWorkTime))
+    }
+
+}
