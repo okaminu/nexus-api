ff2f3152df258b0726c102b82c10192815e0bd2b
Andrius Degutis <andriusdgt@gmail.com>
2020-01-16 18:13:19 +0200

    BA-891: refactoring

--- src/main/kotlin/lt/boldadmin/nexus/api/validator/WorkTimeLengthValidator.kt
+++ src/main/kotlin/lt/boldadmin/nexus/api/validator/WorkTimeLengthValidator.kt
@@ -1,10 +1,11 @@
 package lt.boldadmin.nexus.api.validator
 
-import lt.boldadmin.nexus.api.type.entity.collaborator.Collaborator
+import lt.boldadmin.nexus.api.type.valueobject.TimeRange
 
 class WorkTimeLengthValidator {
 
-    fun isValid(c: Collaborator) = c.workTime.all{ it.endOfDayInMinutes - it.startOfDayInMinutes >= MINIMUM_MINUTES}
+    fun isValid(workTime: Array<TimeRange>) =
+        workTime.all{ it.endOfDayInMinutes - it.startOfDayInMinutes >= MINIMUM_MINUTES}
 
     companion object {
         private val MINIMUM_MINUTES = 15
--- src/test/kotlin/lt/boldadmin/nexus/api/test/unit/validator/WorkTimeLengthValidatorTest.kt
+++ src/test/kotlin/lt/boldadmin/nexus/api/test/unit/validator/WorkTimeLengthValidatorTest.kt
@@ -1,6 +1,5 @@
 package lt.boldadmin.nexus.api.test.unit.validator
 
-import lt.boldadmin.nexus.api.type.entity.collaborator.Collaborator
 import lt.boldadmin.nexus.api.type.valueobject.TimeRange
 import lt.boldadmin.nexus.api.validator.WorkTimeLengthValidator
 import org.junit.jupiter.api.Assertions.assertFalse
@@ -11,30 +10,23 @@ class WorkTimeLengthValidatorTest {
 
     @Test
     fun `Validation passes when all work days time is not shorter than 15 minutes`() {
-        val collaborator = Collaborator().apply {
-            workTime = Array(7) { TimeRange(0, 100) }
-        }
+        val workTime = Array(7) { TimeRange(0, 100) }
 
-        assertTrue(WorkTimeLengthValidator().isValid(collaborator))
+        assertTrue(WorkTimeLengthValidator().isValid(workTime))
     }
 
     @Test
     fun `Validation passes when work days with length of 15 minutes are contained`() {
-        val collaborator = Collaborator().apply {
-            workTime = Array(7) { TimeRange(5, 20) }
-        }
+        val workTime = Array(7) { TimeRange(5, 20) }
 
-        assertTrue(WorkTimeLengthValidator().isValid(collaborator))
+        assertTrue(WorkTimeLengthValidator().isValid(workTime))
     }
 
     @Test
     fun `Validation fails when at least one work day time is shorter than 15 minutes`() {
-        val collaborator = Collaborator().apply {
-            workTime = Array(7) { TimeRange(0, 100) }
-            workTime[2] = TimeRange(20, 30)
-        }
+        val workTime = Array(6) { TimeRange(0, 100) } + TimeRange(20, 30)
 
-        assertFalse(WorkTimeLengthValidator().isValid(collaborator))
+        assertFalse(WorkTimeLengthValidator().isValid(workTime))
     }
 
 }
