c8b3eaa4ed423035d78ff90f8b6859189c8769c5
Andrius Degutis <andriusdgt@gmail.com>
2020-01-17 13:04:48 +0200

    BA-891: validator renamed

--- src/main/kotlin/lt/boldadmin/nexus/api/validator/WorkTimeStartsBeforeEndValidator.kt
+++ src/main/kotlin/lt/boldadmin/nexus/api/validator/WorkTimeStartsBeforeEndValidator.kt
@@ -2,7 +2,7 @@ package lt.boldadmin.nexus.api.validator
 
 import lt.boldadmin.nexus.api.type.valueobject.TimeRange
 
-class WorkTimeValidator {
+class WorkTimeStartsBeforeEndValidator {
 
     fun isValid(workTime: Array<TimeRange>) = workTime.all { it.startOfDayInMinutes <= it.endOfDayInMinutes }
 
