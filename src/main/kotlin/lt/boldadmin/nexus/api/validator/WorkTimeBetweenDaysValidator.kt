0fc44f7516a763f03a74075e8ed7322291543417
Andrius Degutis <andriusdgt@gmail.com>
2020-01-17 14:36:36 +0200

    BA-891: implementation improvements

--- src/main/kotlin/lt/boldadmin/nexus/api/validator/WorkTimeBetweenDaysValidator.kt
+++ src/main/kotlin/lt/boldadmin/nexus/api/validator/WorkTimeBetweenDaysValidator.kt
@@ -6,9 +6,17 @@ class WorkTimeBetweenDaysValidator {
 
     fun isValid(workTime: Array<TimeRange>): Boolean {
         for (i: Int in 0..workTime.size - 2)
-            if (1440 - workTime[i].endOfDayInMinutes + workTime[i + 1].startOfDayInMinutes < 10)
+            if (!(workTime[i] isDistanced workTime[i + 1]))
                 return false
-        return 1440 - workTime[6].endOfDayInMinutes + workTime[0].startOfDayInMinutes >= 10
+        return workTime[6] isDistanced workTime[0]
+    }
+
+    private infix fun TimeRange.isDistanced(that: TimeRange): Boolean =
+        DAY_IN_MINUTES - this.endOfDayInMinutes + that.startOfDayInMinutes >= MINIMUM_MINUTES_BETWEEN_WORK_DAYS
+
+    companion object {
+        private val DAY_IN_MINUTES = 1440
+        private val MINIMUM_MINUTES_BETWEEN_WORK_DAYS = 10
     }
 
 }
