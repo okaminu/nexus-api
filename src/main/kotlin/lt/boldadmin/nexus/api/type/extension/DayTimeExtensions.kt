c99eef82aa635a065dc4fe875f93eb2c95695968
Andrius Degutis <andriusdgt@gmail.com>
2020-01-29 18:27:20 +0200

    BA-891: extension moved to nexus-api

--- src/main/kotlin/lt/boldadmin/nexus/api/type/extension/DayTimeExtensions.kt
+++ src/main/kotlin/lt/boldadmin/nexus/api/type/extension/DayTimeExtensions.kt
@@ -1,9 +1,31 @@
 package lt.boldadmin.nexus.api.type.extension
 
 import lt.boldadmin.nexus.api.type.valueobject.DayTime
+import lt.boldadmin.nexus.api.type.valueobject.TimeRange
 import java.time.DayOfWeek
 import java.util.*
 import kotlin.NoSuchElementException
 
+fun createSortedDaysOfWeek(time: TimeRange) =
+    DayOfWeek.values().map {
+        DayTime(time, false, it.ordinal)
+    }.toSortedSet()
+
 operator fun SortedSet<DayTime>.get(day: DayOfWeek): DayTime =
     this.find { it.dayOfWeekIndex == day.ordinal } ?: throw NoSuchElementException()
+
+fun SortedSet<DayTime>.setEnabled(vararg days: DayOfWeek) = changeEnabledStatus(true, *days)
+
+fun SortedSet<DayTime>.setDisabled(vararg days: DayOfWeek) = changeEnabledStatus(false, *days)
+
+fun SortedSet<DayTime>.setTime(time: TimeRange, day: DayOfWeek) =
+    this.toMutableList().apply {
+        this[day.ordinal] = this[day.ordinal].copy(time = time)
+    }.toSortedSet()
+
+private fun SortedSet<DayTime>.changeEnabledStatus(isEnabled: Boolean, vararg days: DayOfWeek): SortedSet<DayTime> =
+    this.toMutableList().apply {
+        days.forEach { day ->
+            this[day.ordinal] = this[day.ordinal].copy(isEnabled = isEnabled)
+        }
+    }.toSortedSet()
--- src/test/kotlin/lt/boldadmin/nexus/api/test/unit/type/extension/DayTimeExtensionsTest.kt
+++ src/test/kotlin/lt/boldadmin/nexus/api/test/unit/type/extension/DayTimeExtensionsTest.kt
@@ -1,14 +1,16 @@
 package lt.boldadmin.nexus.api.test.unit.type.extension
 
-import lt.boldadmin.nexus.api.type.extension.get
+import lt.boldadmin.nexus.api.type.extension.*
 import lt.boldadmin.nexus.api.type.valueobject.DayTime
-import org.junit.jupiter.api.Assertions.assertEquals
+import lt.boldadmin.nexus.api.type.valueobject.TimeRange
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import java.time.DayOfWeek.*
+import java.util.*
 
-class DayTimeExtensionsTest{
-    
+class DayTimeExtensionsTest {
+
     @Test
     fun `Sorted day times can be retrieved by day`() {
         val firstWorkDay = DayTime(dayOfWeekIndex = MONDAY.ordinal)
@@ -23,6 +25,64 @@ class DayTimeExtensionsTest{
         assertThrows<NoSuchElementException> {
             sortedSetOf(DayTime(dayOfWeekIndex = WEDNESDAY.ordinal))[MONDAY]
         }
+    }
+
+    @Test
+    fun `Creates a sorted set of 7 day times`() {
+        val timeRange = TimeRange(100, 300)
+
+        val sortedDayTimes: SortedSet<DayTime> = createSortedDaysOfWeek(timeRange)
+
+        assertEquals(7, sortedDayTimes.size)
+        assertEquals(timeRange, sortedDayTimes[FRIDAY].time)
+    }
+
+    @Test
+    fun `Sorted day times contain incremented day values`() {
+        val sortedWorkDays = createSortedDaysOfWeek(TimeRange(100, 300))
+
+        assertEquals(4, sortedWorkDays[FRIDAY].dayOfWeekIndex)
+    }
+
+    @Test
+    fun `Changes desired day times to enabled state`() {
+        val timeRange = TimeRange(100, 300)
+
+        val sortedDayTimes: SortedSet<DayTime> = createSortedDaysOfWeek(timeRange).setEnabled(FRIDAY, SUNDAY)
+
+        assertTrue(sortedDayTimes[FRIDAY].isEnabled)
+        assertTrue(sortedDayTimes[SUNDAY].isEnabled)
+        assertFalse(sortedDayTimes[THURSDAY].isEnabled)
+        assertEquals(timeRange, sortedDayTimes[FRIDAY].time)
+        assertEquals(timeRange, sortedDayTimes[SUNDAY].time)
+    }
+
+    @Test
+    fun `Changes desired day times to disabled state`() {
+        val timeRange = TimeRange(100, 300)
+
+        val sortedDayTimes: SortedSet<DayTime> =
+            sortedSetOf(
+                DayTime(timeRange, false, MONDAY.ordinal),
+                DayTime(timeRange, true, TUESDAY.ordinal),
+                DayTime(timeRange, true, WEDNESDAY.ordinal)
+            ).setDisabled(TUESDAY, WEDNESDAY)
+
+        assertFalse(sortedDayTimes[TUESDAY].isEnabled)
+        assertFalse(sortedDayTimes[WEDNESDAY].isEnabled)
+        assertEquals(timeRange, sortedDayTimes[TUESDAY].time)
+        assertEquals(timeRange, sortedDayTimes[WEDNESDAY].time)
+    }
+
+    @Test
+    fun `Changes desired day time`() {
+        val timeRange = TimeRange(100, 300)
+        val newTimeRange = TimeRange(400, 600)
+
+        val sortedDayTimes: SortedSet<DayTime> = createSortedDaysOfWeek(timeRange).setTime(newTimeRange, FRIDAY)
+
+        assertEquals(newTimeRange, sortedDayTimes[FRIDAY].time)
+        assertEquals(sortedDayTimes[THURSDAY].isEnabled, sortedDayTimes[FRIDAY].isEnabled)
     }
 
 }
