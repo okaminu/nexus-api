40ff3860487477c6ea40a9a2fbabce9cd06eb97b
Andrius Degutis <andriusdgt@gmail.com>
2020-01-30 16:27:16 +0200

    BA-891: registered new annotation for validator, second parameter made optional

---
+++ src/main/kotlin/lt/boldadmin/nexus/api/type/annotation/WorkWeekValidInput.kt
@@ -0,0 +1,16 @@
+package lt.boldadmin.nexus.api.type.annotation
+
+import javax.validation.Constraint
+import javax.validation.Payload
+import kotlin.reflect.KClass
+
+@Target(AnnotationTarget.FIELD)
+@Retention(AnnotationRetention.RUNTIME)
+@Constraint(validatedBy = [])
+annotation class WorkWeekValidInput(
+
+    val message: String = "Invalid work week input for Collaborator",
+    val groups: Array<KClass<*>> = [],
+    val payload: Array<KClass<out Payload>> = []
+
+)
