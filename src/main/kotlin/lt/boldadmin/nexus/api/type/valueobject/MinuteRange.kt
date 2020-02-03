c966475fc0207ea902ea34fab42c0a565a9998a6
Andrius Degutis <andriusdgt@gmail.com>
2020-02-03 14:29:18 +0200

    BA-891: naming changes

--- src/main/kotlin/lt/boldadmin/nexus/api/type/valueobject/MinuteRange.kt
+++ src/main/kotlin/lt/boldadmin/nexus/api/type/valueobject/MinuteRange.kt
@@ -1,3 +1,3 @@
 package lt.boldadmin.nexus.api.type.valueobject
 
-data class MinuteRange(var startOfDayInMinutes: Short = 0, var endOfDayInMinutes: Short = 0)
+data class MinuteRange(var start: Short = 0, var end: Short = 0)
