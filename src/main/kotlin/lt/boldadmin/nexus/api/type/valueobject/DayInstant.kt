0a4e09015b09e290ab15986965c8870545d8b6c2
Okaminu <aurimasdgt@gmail.com>
2020-02-05 15:33:20 +0200

    BA-891: renamed day of instant field name

--- src/main/kotlin/lt/boldadmin/nexus/api/type/valueobject/DayInstant.kt
+++ src/main/kotlin/lt/boldadmin/nexus/api/type/valueobject/DayInstant.kt
@@ -2,4 +2,4 @@ package lt.boldadmin.nexus.api.type.valueobject
 
 import java.time.DayOfWeek
 
-data class DayInstant(val dayTimeInMinutes: Short, val day: DayOfWeek)
+data class DayInstant(val dayTimeInMinutes: Short, val dayOfWeek: DayOfWeek)
