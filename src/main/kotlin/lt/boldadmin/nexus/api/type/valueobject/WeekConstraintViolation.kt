a45c157042a087b78daea9bc9134e185d6200215
Andrius Degutis <andriusdgt@gmail.com>
2020-02-05 21:36:03 +0200

    BA-891: properties made public

--- src/main/kotlin/lt/boldadmin/nexus/api/type/valueobject/WeekConstraintViolation.kt
+++ src/main/kotlin/lt/boldadmin/nexus/api/type/valueobject/WeekConstraintViolation.kt
@@ -2,4 +2,4 @@ package lt.boldadmin.nexus.api.type.valueobject
 
 import java.time.DayOfWeek
 
-class WeekConstraintViolation(private val message: String, private val dayOfWeek: DayOfWeek)
+class WeekConstraintViolation(val message: String, val dayOfWeek: DayOfWeek)
