c31746bcf241caf7c64db9652b4b29394585d5b6
Andrius Degutis <andriusdgt@gmail.com>
2020-02-06 21:08:33 +0200

    BA-891: cleanup

--- src/main/kotlin/lt/boldadmin/nexus/api/type/valueobject/DayMinuteInstant.kt
+++ src/main/kotlin/lt/boldadmin/nexus/api/type/valueobject/DayMinuteInstant.kt
@@ -2,4 +2,4 @@ package lt.boldadmin.nexus.api.type.valueobject
 
 import java.time.DayOfWeek
 
-data class DayInstant(val dayTimeInMinutes: Short, val dayOfWeek: DayOfWeek)
+data class DayMinuteInstant(val minutes: Short, val dayOfWeek: DayOfWeek)
--- src/main/kotlin/lt/boldadmin/nexus/api/type/valueobject/MinuteRange.kt
+++ src/main/kotlin/lt/boldadmin/nexus/api/type/valueobject/MinuteRange.kt
@@ -1,3 +1,10 @@
 package lt.boldadmin.nexus.api.type.valueobject
 
-data class MinuteRange(var start: Short = 0, var end: Short = 0)
+data class MinuteRange(var start: Short = 0, var end: Short = 0){
+
+    companion object {
+        const val MIN: Short = 0
+        const val MAX: Short = 1440
+    }
+
+}
