60772896f4b451ecbb0f656e07ede53ead092f7a
Andrius Degutis <andriusdgt@gmail.com>
2020-02-06 14:10:58 +0200

    BA-891: validation endpoint prototyping, Validator.kt -> Sortable.kt

--- src/main/kotlin/lt/boldadmin/nexus/api/service/collaborator/WorkWeekValidatorService.kt
+++ src/main/kotlin/lt/boldadmin/nexus/api/service/collaborator/WorkWeekValidatorService.kt
@@ -1,8 +1,9 @@
 package lt.boldadmin.nexus.api.service.collaborator
 
 import lt.boldadmin.nexus.api.type.valueobject.Day
+import lt.boldadmin.nexus.api.type.valueobject.WeekConstraintViolation
 import java.util.*
 
 interface WorkWeekValidatorService {
-    fun validate(workWeek: SortedSet<Day>)
+    fun getConstraintViolations(workWeek: SortedSet<Day>): Set<WeekConstraintViolation>
 }
