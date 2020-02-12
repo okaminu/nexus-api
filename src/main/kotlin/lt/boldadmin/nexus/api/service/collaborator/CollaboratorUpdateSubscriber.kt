dc01a168487f0f03cfc0df767dfc668ea3f539ec
Andrius Degutis <andriusdgt@gmail.com>
2020-02-12 21:06:35 +0200

    BA-891: collaborator work week can now be updated, various minor fixes, mocks rewrote to mockK

--- src/main/kotlin/lt/boldadmin/nexus/api/service/collaborator/CollaboratorUpdateSubscriber.kt
+++ src/main/kotlin/lt/boldadmin/nexus/api/service/collaborator/CollaboratorUpdateSubscriber.kt
@@ -1,7 +1,9 @@
 package lt.boldadmin.nexus.api.service.collaborator
 
 import lt.boldadmin.nexus.api.type.entity.Collaborator
+import lt.boldadmin.nexus.api.type.valueobject.Day
+import java.util.*
 
 interface CollaboratorUpdateSubscriber {
-    fun notifyBefore(collaborator: Collaborator, attributeName: String, attributeValue: String)
+    fun notifyBefore(collaborator: Collaborator, attributeValue: SortedSet<Day>)
 }
