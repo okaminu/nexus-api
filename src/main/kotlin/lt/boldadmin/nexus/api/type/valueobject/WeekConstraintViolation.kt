14bd48a1c475684e3cf1a27adaeb296366d062e9
Andrius Degutis <andriusdgt@gmail.com>
2020-02-12 13:18:10 +0200

    BA-891: removed custom equals by dayOfWeek

--- src/main/kotlin/lt/boldadmin/nexus/api/type/valueobject/WeekConstraintViolation.kt
+++ src/main/kotlin/lt/boldadmin/nexus/api/type/valueobject/WeekConstraintViolation.kt
@@ -2,10 +2,4 @@ package lt.boldadmin.nexus.api.type.valueobject
 
 import java.time.DayOfWeek
 
-class WeekConstraintViolation(val message: String, val dayOfWeek: DayOfWeek) {
-
-    override fun equals(other: Any?): Boolean = other is WeekConstraintViolation && this.dayOfWeek == other.dayOfWeek
-
-    override fun hashCode(): Int = dayOfWeek.ordinal.hashCode()
-
-}
+data class WeekConstraintViolation(val message: String, val dayOfWeek: DayOfWeek)
