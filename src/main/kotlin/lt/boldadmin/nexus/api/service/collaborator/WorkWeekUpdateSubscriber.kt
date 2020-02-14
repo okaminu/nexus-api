e0e0f82dcc03c943b74031cdfa705238a1b35b0c
Andrius Degutis <andriusdgt@gmail.com>
2020-02-14 15:05:51 +0200

    BA-891: API simplified

--- src/main/kotlin/lt/boldadmin/nexus/api/service/collaborator/WorkWeekUpdateSubscriber.kt
+++ src/main/kotlin/lt/boldadmin/nexus/api/service/collaborator/WorkWeekUpdateSubscriber.kt
@@ -1,9 +1,8 @@
 package lt.boldadmin.nexus.api.service.collaborator
 
-import lt.boldadmin.nexus.api.type.entity.Collaborator
 import lt.boldadmin.nexus.api.type.valueobject.DayMinuteInterval
 import java.util.*
 
 interface WorkWeekUpdateSubscriber {
-    fun notifyBefore(collaborator: Collaborator, workWeek: SortedSet<DayMinuteInterval>)
+    fun notifyBefore(collaboratorId: String, workWeek: SortedSet<DayMinuteInterval>)
 }
