6fd9a8ad6f88bdd06a3455f49bdbcbcdc9f18936
Andrius Degutis <andriusdgt@gmail.com>
2020-02-21 13:27:23 +0200

    BA-891: lint issues fixed

--- src/main/kotlin/lt/boldadmin/nexus/api/type/valueobject/location/Location.kt
+++ src/main/kotlin/lt/boldadmin/nexus/api/type/valueobject/location/Location.kt
@@ -1,5 +1,3 @@
 package lt.boldadmin.nexus.api.type.valueobject.location
 
-import lt.boldadmin.nexus.api.type.valueobject.location.Coordinates
-
 data class Location(val coordinates: Coordinates = Coordinates(), val radiusInMeters: Int = 0)
