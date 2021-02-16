4d8a439512e7b50ff42cea891b9886f48689fb35
Okaminu <aurimasdgt@gmail.com>
2021-02-16 16:04:06 +0200

    removed logging by messages

--- src/main/kotlin/lt/boldadmin/nexus/api/event/publisher/CollaboratorMessagePublisher.kt
+++
@@ -1,7 +0,0 @@
-package lt.boldadmin.nexus.api.event.publisher
-
-import lt.boldadmin.nexus.api.type.valueobject.Message
-
-interface CollaboratorMessagePublisher {
-    fun publish(message: Message)
-}
